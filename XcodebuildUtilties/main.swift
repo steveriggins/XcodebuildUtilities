77302ff8ab3fb6fc85acd08c5c4f80eaf0a795f3
Douglas Sjoquist <dwsjoquist@sunetos.com>
2016-02-01 20:59:37 -0500

    Initial Commit

---
+++ XcodebuildUtilties/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  XcodebuildUtilties
+//
+//  Created by Douglas Sjoquist on 2/1/16.
+//  Copyright © 2016 Ivy Gulch. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
