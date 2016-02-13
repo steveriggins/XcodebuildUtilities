7c3ef9690cbe40d4a0b51a9654ad99d4beafd0cb
Douglas Sjoquist <dwsjoquist@sunetos.com>
2016-02-13 13:42:36 -0500

    add XML support for XCTestCaseResult

--- XCTestsToJUnitTests/XCTestCaseResultTests.swift
+++ XCTestsToJUnitTests/XCTestCaseResultTests.swift
@@ -59,5 +59,34 @@ class XCTestCaseResultTests: XCTestCase {
         XCTAssertEqual(1, testCase.failureMessages.count)
 
     }
-    
+
+    func testXMLElement() {
+        let expectedLogLines = [
+            "Line A",
+            "/Users/Shared/Jenkins/Home/jobs/dwsjoquist testing/workspace/ASDA-Tests/Common/DummySwiftAllFailuresTests.swift:15: error: -[ASDA_Tests.DummySwiftAllFailuresTests testFailure1] : failed - Failure 1",
+            "Line B"
+        ]
+
+        let dummySuite = XCTestSuiteResult(testSummary:XCTestSummaryResult(), suiteName:"suiteName")
+        let testCase = XCTestCaseResult(testSuite:dummySuite,methodName:"methodName",duration:123.4)
+
+        for logLine in expectedLogLines {
+            testCase.processLog(logLine)
+        }
+
+        // <testcase classname='DummyObjCSomeFailuresTests' name='testFailure1' time='0.001'>
+        // <failure message='failed - Failure 1' type='Failure'>/Users/Shared/Jenkins/Home/jobs/dwsjoquist testing/workspace/ASDA-Tests/Common/DummyObjCSomeFailuresTests.m:19</failure>
+        // </testcase>
+        // <testcase classname='DummyObjCSomeFailuresTests' name='testSuccess1' time='0.0' />
+        let xmlElement = testCase.xmlElement()
+
+        XCTAssertEqual("testcase", xmlElement.name)
+        expectAttribute(xmlElement, name:"classname", stringValue:"suiteName")
+        expectAttribute(xmlElement, name:"name", stringValue:"methodName")
+        expectAttribute(xmlElement, name:"time", stringValue:"123.4")
+        expectAttribute(xmlElement, name:"status", stringValue:"Failure")
+
+        XCTAssertEqual(1, xmlElement.children!.count)
+    }
+
 }
